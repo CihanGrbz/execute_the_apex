929ec5ea159e1011abd8992bc2dec92081057cd0
Mustafa Gürbüz <mustafa-cihan@hotmail.com>
2024-06-19 12:39:20 +0200

    GOD-14-enemy-intentions-and-polish
    - i'm a genius

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,8 @@
 class_name Player
 extends Node2D
 
+const WHITE_SPRITE_MATERIAL := preload("res://art/white_sprite_material.tres")
+
 @export var stats: CharacterStats: set = set_character_stats
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
@@ -14,6 +16,7 @@ func set_character_stats(value: CharacterStats) -> void:
 	
 	update_player()
 	
+
 func update_player() -> void:
 	if not stats is CharacterStats:
 		return
@@ -23,15 +26,28 @@ func update_player() -> void:
 	sprite_2d.texture = stats.art
 	update_stats()
 	
+
 func update_stats() -> void:
 	stats_ui.update_stats(stats)
 	
+
 func take_damage(damage: int) -> void:
 	if stats.health <= 0:
 		return
 	
-	stats.take_damage(damage)
+	sprite_2d.material = WHITE_SPRITE_MATERIAL
 	
-	if stats.health <= 0:
-		Events.player_died.emit()
-		queue_free()
+	var tween := create_tween()
+	tween.tween_callback(Shaker.shake.bind(self, 16, 0.15))
+	tween.tween_callback(stats.take_damage.bind(damage))
+	tween.tween_interval(0.17)
+	
+	tween.finished.connect(
+		func():
+			sprite_2d.material = null
+			
+			if stats.health <= 0:
+				Events.player_died.emit()
+				queue_free()
+	)
+	
